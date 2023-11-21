078fc3752b21f99262742beb1b70d11dd46e1079
Altan Nagji <altannagji@gmail.com>
2023-11-21 00:39:51 -0600

    feat: Update tbb_template

--- benchmarks/tbb_template.cpp
+++ benchmarks/tbb_template.cpp
@@ -5,8 +5,13 @@
 #include <chrono>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 using namespace std;
+using namespace oneapi::tbb;
+const int num_threads = 4;        // Number of threads in the pool
+const int num_tasks = 10000;     // Total number of tasks to execute
+const int tasks_per_batch = 1000; // Number of tasks in each batch
 
 
 class ThreadSleep {
@@ -17,9 +22,7 @@ class ThreadSleep {
 					exit(1);
 			}
 			for(size_t i = r.begin(); i != r.end(); ++i) {
-				printf("%zu is going to sleep.\n", i);
 				this_thread::sleep_for(chrono::milliseconds(10));
-				printf("%zu has woken up.\n", i);
 			}
 		}
 };
@@ -29,10 +32,9 @@ void ParallelThreadSleep(size_t n) {
 	oneapi::tbb::parallel_for(
 		oneapi::tbb::blocked_range<size_t>(0, n),
 		ThreadSleep(),
-		oneapi::tbb::simple_partitioner() 
+		oneapi::tbb::simple_partitioner()
 		/*
-			Simple Partitioner (default grainsize = 1) 
-			ensures each item a new sleep task is submitted
+			Simple Partitioner ensures each item a new sleep task is submitted
 			per item in the range.
 		*/
 	);
@@ -40,14 +42,27 @@ void ParallelThreadSleep(size_t n) {
 
  
 int main() {
-	
-	auto start_time = chrono::high_resolution_clock::now();
-	ParallelThreadSleep(1000);
-	auto end_time = chrono::high_resolution_clock::now();
-    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
-    cout << "Throughput benchmark results: " << endl;
+	global_control global_limit(
+		global_control::max_allowed_parallelism, 
+		num_threads
+	);
 
+	cout << "Template benchmark results: " << endl;
+	double total_duration = 0;
+	int iter = 0;
+	int num_tasks_executed = 0;
+	for(int i = 0; i < num_tasks; i += tasks_per_batch, ++iter) {
+        int batch_size = std::min(tasks_per_batch, num_tasks - i);
+		num_tasks_executed += batch_size;
+		auto start_time = chrono::high_resolution_clock::now();
+		ParallelThreadSleep(batch_size);
+		auto end_time = chrono::high_resolution_clock::now();
+		auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+		total_duration += elapsed_time.count();
+		cout << "Iteration " << iter << "(" << i << "/" << num_tasks << ") total_duration: " << elapsed_time.count() << "ms.\n";
+	}
+	cout << "Average time per task: " << total_duration/num_tasks_executed << " ms per task.\n";
 	return 0;
 	
 }
