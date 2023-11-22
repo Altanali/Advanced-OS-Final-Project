1a98f556e4ba9fd0d98a8f85c2b45be85b6aeadf
RikGhosh487 <rikghosh487@gmail.com>
2023-11-22 13:08:05 -0600

    Moved everything to Header files

--- src/thread_safe_queue.hpp
+++ src/thread_safe_queue.hpp
@@ -8,7 +8,10 @@
 
 namespace pool
 {
-    template <typename T, typename Lock = std::mutex>
+    /**
+     * @brief A thread-safe and work-stealing queue implementation
+     */
+    template <typename T>
     class ThreadSafeQueue
     {
     public:
@@ -16,36 +19,146 @@ namespace pool
         using size_type = typename std::deque<T>::size_type;
 
         // Default constructor
-        ThreadSafeQueue();
+        ThreadSafeQueue() = default;
 
-        // Add an element to the back of the queue
-        void pushBack(T &&value);
+        /**
+         * @brief Add an element to the back of the queue
+         *
+         * @tparam T generic type for the element being stored in the queue
+         * @param value the element to be pushed to the back of the queue
+         */
+        void pushBack(T &&value)
+        {
+            std::scoped_lock lock(mutex_);
+            data_.push_back(std::forward<T>(value));
+        }
 
-        // Add an element to the front of the queue
-        void pushFront(T &&value);
+        /**
+         * @brief Add an element to the front of the queue
+         *
+         * @tparam T generic type for the element being stored in the queue
+         * @param value the element to be pushed to the front of the queue
+         */
+        void pushFront(T &&value)
+        {
+            std::scoped_lock lock(mutex_);
+            data_.push_front(std::forward<T>(value));
+        }
 
-        // Check if the queue is empty
-        [[nodiscard]] bool empty() const;
+        /**
+         * @brief Check if the queue is empty
+         *
+         * @return boolean to indicate if the queue is empty
+         */
+        [[nodiscard]] bool empty() const
+        {
+            std::scoped_lock lock(mutex_);
+            return data_.empty();
+        }
 
-        // Remove and return an element from the front of the queue
-        [[nodiscard]] std::optional<T> popFront();
+        /**
+         * @brief Remove and return an element from the front of the queue
+         *
+         * @return the element previously stored at the front of the queue
+         */
+        [[nodiscard]] std::optional<T> popFront()
+        {
+            std::scoped_lock lock(mutex_);
+            if (data_.empty())
+            {
+                return std::nullopt;
+            }
 
-        // Remove and return an element from the back of the queue
-        [[nodiscard]] std::optional<T> popBack();
+            auto front = std::move(data_.front());
+            data_.pop_front();
+            return front;
+        }
 
-        // Remove and return an element from the back of the queue (steal spot)
-        [[nodiscard]] std::optional<T> steal();
+        /**
+         * @brief Remove and return an element from the back of the queue
+         *
+         * @return the element previously stored at the back of the queue
+         */
+        [[nodiscard]] std::optional<T> popBack()
+        {
+            std::scoped_lock lock(mutex_);
+            if (data_.empty())
+            {
+                return std::nullopt;
+            }
 
-        // Move an item to the front of the queue
-        void moveToFront(const T &item);
+            auto back = std::move(data_.back());
+            data_.pop_back();
+            return back;
+        }
 
-        // Copy the front item, move it to the back, and return the copied item
-        [[nodiscard]] std::optional<T> copyFrontAndMoveToBack();
+        /**
+         * @brief Remove and return an element from the back of the queue (steal spot)
+         *
+         * @return the element previously stored at the back of the queue
+         */
+        [[nodiscard]] std::optional<T> steal()
+        {
+            std::scoped_lock lock(mutex_);
+            if (data_.empty())
+            {
+                return std::nullopt;
+            }
+
+            auto back = std::move(data_.back());
+            data_.pop_back();
+            return back;
+        }
+
+        /**
+         * @brief Move an item to the front of the queue
+         *
+         * @tparam T generic type for the element being stored in the queue
+         * @param item the element to be pushed to the front of the queue
+         */
+        void moveToFront(const T &item)
+        {
+            std::scoped_lock lock(mutex_);
+            auto iter = std::find(data_.begin(), data_.end(), item);
+
+            if (iter != data_.end())
+            {
+                std::ignore = data_.erase(iter);
+            }
+
+            data_.push_front(item);
+        }
+
+        /**
+         * @brief Copy the front item, move it to the back, and return the copied item
+         *
+         * @return the element that was previously at the front of the queue
+         */
+        [[nodiscard]] std::optional<T> copyFrontAndMoveToBack()
+        {
+            std::scoped_lock lock(mutex_);
+            if (data_.empty())
+            {
+                return std::nullopt;
+            }
+
+            auto front = data_.front();
+            data_.pop_front();
+            data_.push_back(front);
+            return front;
+        }
 
     private:
+        /**
+         * @brief internal data structure to store the data
+         */
         std::deque<T> data_{};
-        mutable Lock mutex_{};
+
+        /**
+         * @brief mutex for locking
+         */
+        mutable std::mutex mutex_{};
     };
-}
+} // namespace pool
 
 #endif // !THREAD_SAFE_QUEUE_H
