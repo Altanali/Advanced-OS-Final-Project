33fb0539109d07a3588d98c21d25e254e22a523c
Altan Nagji <altannagji@gmail.com>
2023-11-23 17:43:19 -0600

    fix: Tbb blocking tasks varries number of tasks blocked

--- benchmarks/tbb_blocking_tasks.cpp
+++ benchmarks/tbb_blocking_tasks.cpp
@@ -17,6 +17,7 @@ using namespace oneapi::tbb;
 const int max_threads = 16;        // Number of threads in the pool
 const int num_tasks = 1000;      // Total number of tasks to execute
 const int numRepeats = 3;
+int blocked_tasks[num_tasks];
 
 class ThreadTask{
 	public:
@@ -24,10 +25,9 @@ class ThreadTask{
 			assert(r.size() == 1);
 			int item = r.begin();
 			//If task number == 2, block; else perform GEMM computation. 
-			if(item % 2) {
-				this_thread::sleep_for(chrono::milliseconds(20));
+			if(blocked_tasks[item]) {
+				this_thread::sleep_for(chrono::milliseconds(50));
 			} else {
-				
 				int m = 100;
 				int n = 100;
 				int k = 100;
@@ -80,26 +80,36 @@ int main() {
 	double total_duration = 0;
 	int iter = 0;
 	int num_tasks_executed = 0;
-	for(int num_threads = 1; num_threads <= max_threads; num_threads*=2) {
-
+	// for(int num_threads = 1; num_threads <= max_threads; num_threads*=2) {
+		int num_threads = 8;
 		global_control global_limit(
 			global_control::max_allowed_parallelism, 
 			num_threads
 		);
+		
+		for(double percent_blocked = 0; percent_blocked <= 1; percent_blocked += 0.2) {
+			int num_blocked_tasks = (int)floor(num_tasks*percent_blocked);
+			cout << "Blocking " << num_blocked_tasks << " tasks.\n";
+			for(int repeat = 0; repeat < numRepeats; ++repeat, ++iter) {
 
-		for(int repeat = 0; repeat < numRepeats; ++repeat, ++iter) {
-			num_tasks_executed += num_tasks;
-			auto start_time = chrono::high_resolution_clock::now();
-			ParallelThreadSleep(num_tasks);
-			auto end_time = chrono::high_resolution_clock::now();
-			auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-			total_duration += elapsed_time.count();
+				num_tasks_executed += num_tasks;
+				for(int block = 0; block < num_tasks; ++block) {
+					blocked_tasks[block] = block <= num_tasks_executed;
+				}
+				random_shuffle(blocked_tasks, blocked_tasks + num_tasks);
+				auto start_time = chrono::high_resolution_clock::now();
+				ParallelThreadSleep(num_tasks);
+				auto end_time = chrono::high_resolution_clock::now();
+				auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+				total_duration += elapsed_time.count();
+			}
+			cout << "Thread Count: " << num_threads << endl;
+			cout << "Percent Tasks Blocked: " << percent_blocked << endl;
+			cout << "Average time per task: " << total_duration/num_tasks_executed << " ms per task.\n";
+			cout << "Tasks per second: " << num_tasks_executed/total_duration*1000 << endl;
+			cout << endl;
 		}
-		cout << "Thread Count: " << num_threads << endl;
-		cout << "Average time per task: " << total_duration/num_tasks_executed << " ms per task.\n";
-		cout << "Tasks per second: " << num_tasks_executed/total_duration*1000 << endl;
-
-	}
+	// }
 	return 0;
 	
 }
