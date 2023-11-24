96c56103969ef181ff5e8dc2a0872541a307ff6a
Altan Nagji <altannagji@gmail.com>
2023-11-23 19:50:11 -0600

    fix: Bug where all tasks ended up blocking

--- benchmarks/boost_blocking_tasks.cpp
+++ benchmarks/boost_blocking_tasks.cpp
@@ -7,11 +7,9 @@
 #include <boost/asio/thread_pool.hpp>
 #include "tasks.hpp"
 #include <stdlib.h>
-
 using namespace std;
 using namespace boost::asio;
 
-const int max_threads = 16;        // Number of threads in the pool
 const int num_tasks = 1000;      // Total number of tasks to execute
 const int numRepeats = 3;
 int blocked_tasks[num_tasks];
@@ -21,7 +19,7 @@ int blocked_tasks[num_tasks];
 void task(int idx)
 {
 			if(blocked_tasks[idx]) {
-				std::this_thread::sleep_for(std::chrono::milliseconds(50));
+				std::this_thread::sleep_for(std::chrono::milliseconds(100));
 			} else {
 				int m = 100;
 				int n = 100;
@@ -55,43 +53,38 @@ int main()
 
     std::cout << "Blocking/Nonblocking Benchmark Results:" << std::endl;
 	int num_threads = 8;
-	int num_tasks_executed;
 	for(double percent_blocked = 0; percent_blocked <= 1; percent_blocked += 0.2) {
 		double total_duration = 0;
 		int num_tasks_executed = 0;
 		int num_blocked_tasks = (int)floor(num_tasks*percent_blocked);
 		cout << "Blocking " << num_blocked_tasks << " tasks.\n";
-        for (int repeat = 0; repeat < numRepeats; ++repeat)
-        {
-            thread_pool threadPool(num_threads);
-			for(int repeat = 0; repeat < numRepeats; ++repeat) {
+		for(int repeat = 0; repeat < numRepeats; ++repeat) {
+			thread_pool threadPool(num_threads);
 
-				num_tasks_executed += num_tasks;
-				for(int block = 0; block < num_tasks; ++block) {
-					blocked_tasks[block] = block <= num_tasks_executed;
-				}
-				random_shuffle(blocked_tasks, blocked_tasks + num_tasks);
-				auto start_time = std::chrono::high_resolution_clock::now();
-            	// Measure the performance of tasks
-				auto startTime = std::chrono::high_resolution_clock::now();
-				for (int i = 0; i < num_tasks; ++i)
-				{
-					post(threadPool, bind(&task, i));
-				}
-
-				threadPool.join();
-
-				auto end_time = std::chrono::high_resolution_clock::now();
-				auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-				total_duration += elapsed_time.count();
+			num_tasks_executed += num_tasks;
+			for(int block = 0; block < num_tasks; ++block) {
+				blocked_tasks[block] = block < num_tasks ? 0 : 1;
 			}
+			random_shuffle(blocked_tasks, blocked_tasks + num_tasks);
+			auto start_time = std::chrono::high_resolution_clock::now();
+			// Measure the performance of tasks
+			for (int i = 0; i < num_tasks; ++i)
+			{
+				post(threadPool, bind(&task, i));
+			}
+
+			threadPool.join();
+
+			auto end_time = std::chrono::high_resolution_clock::now();
+			auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+			total_duration += elapsed_time.count();
 		}
 
-			std::cout << "Thread Count: " << num_threads << endl;
-			std::cout << "Percent Tasks Blocked: " << percent_blocked << endl;
-			std::cout << "Average time per task: " << total_duration/num_tasks_executed << " ms per task.\n";
-			std::cout << "Tasks per second: " << num_tasks_executed/total_duration*1000 << endl;
-			std::cout << endl;
+		std::cout << "Thread Count: " << num_threads << endl;
+		std::cout << "Percent Tasks Blocked: " << percent_blocked << endl;
+		std::cout << "Average time per task: " << total_duration/num_tasks_executed << " ms per task.\n";
+		std::cout << "Tasks per second: " << num_tasks_executed/total_duration*1000 << endl;
+		std::cout << endl;
     }
 
     return 0;
--- benchmarks/tbb_blocking_tasks.cpp
+++ benchmarks/tbb_blocking_tasks.cpp
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "tasks.hpp"
-
+#include <random>
 //Vary percent of blocking tasks
 //Measure: 
 //	Time per task, or per batch
@@ -16,6 +16,7 @@ using namespace std;
 using namespace oneapi::tbb;
 const int num_tasks = 1000;      // Total number of tasks to execute
 const int numRepeats = 3;
+const int num_threads = 8;
 int blocked_tasks[num_tasks];
 
 class ThreadTask{
@@ -25,7 +26,7 @@ class ThreadTask{
 			int item = r.begin();
 			//If task number == 2, block; else perform GEMM computation. 
 			if(blocked_tasks[item]) {
-				this_thread::sleep_for(chrono::milliseconds(50));
+				this_thread::sleep_for(chrono::milliseconds(100));
 			} else {
 				int m = 100;
 				int n = 100;
@@ -78,7 +79,7 @@ int main() {
 	cout << "Template benchmark results: " << endl;
 	int iter = 0;
 	// for(int num_threads = 1; num_threads <= max_threads; num_threads*=2) {
-		int num_threads = 8;
+
 		global_control global_limit(
 			global_control::max_allowed_parallelism, 
 			num_threads
@@ -94,10 +95,12 @@ int main() {
 
 				num_tasks_executed += num_tasks;
 				for(int block = 0; block < num_tasks; ++block) {
-					blocked_tasks[block] = block <= num_tasks_executed;
+					blocked_tasks[block] = block < num_tasks ? 0 : 1;
 				}
-				random_shuffle(blocked_tasks, blocked_tasks + num_tasks);
-				auto start_time = chrono::high_resolution_clock::now();
+				std::random_device rd;
+				std::mt19937 g(rd());
+
+				std::shuffle(blocked_tasks, blocked_tasks + num_tasks, g);				auto start_time = chrono::high_resolution_clock::now();
 				ParallelThreadSleep(num_tasks);
 				auto end_time = chrono::high_resolution_clock::now();
 				auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
